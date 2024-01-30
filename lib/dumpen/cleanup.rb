3e6ae9dc09d9119fcc1eb8412bf67b94c3913d18
Manfred Stienstra <manfred@fngtps.com>
2024-01-30 12:20:20 +0100

    Explain Cleanup class and improve formatting.

--- lib/dumpen/cleanup.rb
+++ lib/dumpen/cleanup.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'find'
 
 class Dumpen
+  # Removes all dumps that are older than 14 days from a path.
   class Cleanup < Dumper
     KEEP_DAYS = 14
 
@@ -11,14 +14,14 @@ class Dumpen
 
     def run
       Find.find(path) do |filename|
-        if File.file?(filename)
-          basename = File.basename(filename, '.sql')
-          date = Date.parse(basename)
-          age_in_days = (Date.today - date).to_i
-          if age_in_days > KEEP_DAYS
-            Dumpen.logger.info("Cleaning up `#{filename}' because it's #{age_in_days} days old")
-            FileUtils.rm_f(filename)
-          end
+        next unless File.file?(filename)
+
+        basename = File.basename(filename, '.sql')
+        date = Date.parse(basename)
+        age_in_days = (Date.today - date).to_i
+        if age_in_days > KEEP_DAYS
+          Dumpen.logger.info("Cleaning up `#{filename}' because it's #{age_in_days} days old")
+          FileUtils.rm_f(filename)
         end
       end
     end
